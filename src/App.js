53b9c8cf26ec5ef96fa8de0783000cb56022b21d
Mousumi Khanam <mousumikhanam99@gmail.com>
2023-10-18 10:00:07 +0530

    feat: created ui for calculator

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="outerbox">
+        <div className="screen">
+          <input type="number" className="calculation" />
+        </div>
+        <div className="operators">
+          <p className='operatorstyle1'>+</p>
+          <p className='operatorstyle1'>-</p>
+          <p className='operatorstyle1'>*</p>
+          <p className='operatorstyle1'>/</p>
+        </div>
+        <div className="numbers">
+          <div className='leftnum'>
+            <div className='numstyle'>
+              <p className='operatorstyle'>1</p>
+              <p className='operatorstyle'>2</p>
+              <p className='operatorstyle'>3</p>
+            </div>
+            <div className='numstyle'>
+              <p className='operatorstyle'>4</p>
+              <p className='operatorstyle'>5</p>
+              <p className='operatorstyle'>6</p>
+            </div>
+            <div className='numstyle'>
+              <p className='operatorstyle'>7</p>
+              <p className='operatorstyle'>8</p>
+              <p className='operatorstyle'>9</p>
+            </div>
+            <div className='numstyle2'>
+              <p></p>
+              <p className='operatorstyle'>0</p>
+              <p className='operatorstyle'>AC</p>
+            </div>           
+          </div>
+          <div className='equals'>
+            <p className="equalstyle">=</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
